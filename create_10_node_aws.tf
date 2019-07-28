6607ae0e912cc795bad4900b4da8bc3f1f5ac2fe
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-07-27 19:37:40 -0500

    add some documentations

--- create_10_node_aws.tf
+++ create_10_node_aws.tf
@@ -1,7 +1,12 @@
+# declaring the provider with the region
 provider "aws" {
   region = "us-west-2"
 }
 
+# In AWS EC2, IAM, VPC and etc all are resource
+# ami- is the EC2 AMI, you can get it from AWS console
+# count- number of instance, you want to create
+# instance_type-type of the instance
 resource "aws_instance" "example" {
   count=0
   ami           = "ami-0f2176987ee50226e"
