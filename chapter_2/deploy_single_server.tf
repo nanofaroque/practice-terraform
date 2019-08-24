71cc470cf80d526af0b53a267e355ad74e91e261
The Omar <faroque.ae@gmail.com>
2019-08-23 22:46:44 -0500

    single server deploy with aws security group

---
+++ chapter_2/deploy_single_server.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "instance_security"
+  }
+}
+resource "aws_instance" "single_server" {
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  //add a name of the instance
+  tags =  {
+    Name = "terraform_single_server"
+  }
+}
